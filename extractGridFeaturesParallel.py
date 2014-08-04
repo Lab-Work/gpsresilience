5bc24e385c31979a14ade26ec22bd328686d4a59
Brian Donovan <briandonovan100@gmail.com>
2014-08-04 00:46:30 -0500

    Refactored extractGridFeaturesParallel.py to use multiprocessing.Pool instead of multiprocessing.Process.  Cleaned up code.

--- extractGridFeaturesParallel.py
+++ extractGridFeaturesParallel.py
@@ -3,56 +3,59 @@ import os
 import shutil
 from datetime import datetime
 from grid import *
-#from cluster import *
 from regions import *
-from multiprocessing import Process
-from time import sleep
+from multiprocessing import Pool
 import shutil
 
 start_time = datetime.now()
 
 
+TMP_DIR = "working_space"
+FINAL_OUTPUT_DIR = "4year_features"
+NUM_PROCESSORS = 8
 
 
-class ParseFileProcess(Process):
-	def __init__(self, infile, tmp_dir, slice_id):
-		super(ParseFileProcess, self).__init__()
-		filename = tmp_dir + "/slice_" + str(slice_id)
-		shutil.rmtree(filename, ignore_errors=True)
-		os.mkdir(filename)
-		self.gridSystem = RegionSystem(filename)
-		self.infile = infile
-		
+def processMonth((year, month, slice_id)):
+	infile = "../new_chron/FOIL" + str(year) + "/trip_data_" + str(month) + ".csv"	
 	
-	def run(self):
-		logMsg('Parsing file ' + self.infile)
-		r = csv.reader(open(self.infile, 'r'))		
-		i = 0
-		header = True
-		for line in r:
-			if(header):
-				Trip.initHeader(line)
-				header = False
-			else:
-				trip = None
-				try:
-					trip = Trip(line)
-				except ValueError:
-					pass
-				
-				
-				if(trip!= None and (self.y!="FOIL" + str(trip.date.year) or self.n!= trip.date.month)):
-					trip.has_other_error = True
-				
-				self.gridSystem.record(trip)
+	outdir = TMP_DIR + "/slice_" + str(slice_id)
+	shutil.rmtree(outdir, ignore_errors=True)
+	os.mkdir(outdir)
+	gridSystem = RegionSystem(outdir)
+	
+	logMsg('Parsing file ' + infile)
+	
+	with open(infile, 'r') as filePointer:
+		csvReader = csv.reader(filePointer)
+		header = csvReader.next()
+		Trip.initHeader(header)
+		
+		for line in csvReader:
 
-		self.gridSystem.close()
+			try:
+				trip = Trip(line) #Parse the csv line into a trip object
+			except ValueError:
+				trip = None      #A trip of None indicates a parsing error
+				
+			
+			#Ignore trips that are placed in the wrong month file
+			if(trip!= None and (year!=trip.date.year or month!=trip.date.month)):
+				trip.has_other_error = True
+			
+			#Record the trip - if trip==None, an error will be recorded
+			gridSystem.record(trip)
+	
+	gridSystem.close()
+	
+	return outdir
 
 
-def mergeTempFiles(tmp_dir, out_dir):
+def mergeTempFiles(slice_dirs, out_dir):
 	logMsg("Merging tmp files")
 	shutil.rmtree(out_dir, ignore_errors=True)
 	os.mkdir(out_dir)
+	
+	#Files that we care about (one for each type of feature):
 	#count_features.csv    errors.csv           miles_features.csv  pace_var_features.csv
 	#drivers_features.csv  global_features.csv  pace_features.csv
 	feature_types = ["count", "miles", "pace_var", "drivers", "global", "pace"]
@@ -61,87 +64,65 @@ def mergeTempFiles(tmp_dir, out_dir):
 	for ft in feature_types:
 		fname = out_dir + "/" + ft + "_features.csv"
 		out_file_pointers[ft] = open(fname, 'w')
-		
-	for slice_id in range(48):
+	
+	
+	first_slice = True
+	for slice_dir in slice_dirs:
 		for ft in feature_types:
 			try:
-				fname = tmp_dir + "/slice_" + str(slice_id) + "/" + ft + "_features.csv"
+				fname = slice_dir + "/" + ft + "_features.csv"
 				infile = open(fname, 'r')
 				
-				if(slice_id!=0):
+				
+				#If this is the first file, the header should be copied to the output file
+				#Otherwise, it should be skipped
+				if(not first_slice):
 					infile.next()
+
 				for line in infile:
 					out_file_pointers[ft].write(line)
 			except:
 				pass
+
+		#First slice is complete - headers should not be copied anymore
+		first_slice = False
 			
 
 
-#grid2
-#gridSystem = GridSystem(-74.04, -73.775, 5, 40.63, 40.835, 5)
-#gridname = "grid2"
 
-#grid3
-#gridSystem = GridSystem(-74.02, -73.938, 4, 40.7, 40.815, 6)
-#gridname = "grid3"
-
-#cluster1
-#gridSystem = ClusterSystem("cluster1/clusters.csv")
-#gridname = "cluster1"
-
-
-tmp_dir = "working_space3"
-shutil.rmtree(tmp_dir, ignore_errors=True)
-os.mkdir(tmp_dir)
-
-invalids = 0
-
-NUM_PC = 8
-
-workers = [None] * NUM_PC
-
-
-slice_id = 0
-for y in ["FOIL2010", "FOIL2011", "FOIL2012", "FOIL2013"]:
-	for n in range(1,13):
-		filename = "../../new_chron/" + y + "/trip_data_" + str(n) + ".csv"		
-		
-		created_job = False
-		#Polling - see if any workers are ready to accept a new job
-		while(not created_job):
-			for i in range(NUM_PC):
-				if(workers[i]==None or not workers[i].is_alive()):
-					workers[i] = ParseFileProcess(filename, tmp_dir, slice_id)
-					workers[i].y = y
-					workers[i].n = n
-					workers[i].start()
-					created_job = True
-					break
-				
-			#Sleep for 1 second so the polling doesn't hog a CPU
-			if(not created_job):
-				sleep(1)
-		
-		slice_id += 1
-		
-
-logMsg("Joining last workers.")
-for w in workers:
-	if(w!=None and w.is_alive()):
-		w.join()
-
-logMsg("Merging output files")
-mergeTempFiles(tmp_dir, "4year_featuresz")
+def sliceIterator():
+	slice_id = 0
+	for year in [2010, 2011, 2012, 2013]:
+		for month in range(1, 13):
+			#This tuple represents the arguments to processMonth()
+			yield (year, month, slice_id)
+			slice_id += 1
 	
-logMsg("Done.")	
-
-"""
-filename = "../../new_chron/" + "FOIL2010" + "/trip_data_" + str(1) + ".csv"
-w = ParseFileProcess(filename, tmp_dir, 0)
-w.y = "FOIL2010"
-w.n = 1
-w.start()
 
 
-w.join()
-"""
+
+#########################################################################################################
+################################### MAIN CODE BEGINS HERE ###############################################
+#########################################################################################################
+if(__name__=="__main__"):
+	#Setup temporary directory to store intermediate results for each month
+	logMsg("Creating working directory for temp files...")
+	shutil.rmtree(TMP_DIR, ignore_errors=True)
+	os.mkdir(TMP_DIR)
+	
+	#Run the main code on each month in parallel (to the extent possible on this machine)
+	#Each month will get a subdirectory inside the temporary directory
+	logMsg("Processing months in parallel (" + str(NUM_PROCESSORS) + " cores)")
+	pool = Pool(NUM_PROCESSORS) #Initialize the pool
+	slice_dirs = pool.map(processMonth, sliceIterator()) #Map the main function onto each month (slice) in parallel
+	#slice_dirs contains all of the intermediate subdirectories.  These will be merged in the next step
+	
+	
+	#Merge intermediate results into large files in one final output folder
+	logMsg("Merging output files")
+	mergeTempFiles(slice_dirs, FINAL_OUTPUT_DIR)
+	
+	logMsg("Cleaning up")
+	shutil.rmtree(TMP_DIR, ignore_errors=True)	
+	
+	logMsg("Done.")	
