4c09171736acd48e0c784804f1349df355e02dbc
Brian Donovan <briandonovan100@gmail.com>
2015-03-03 15:36:43 -0600

    Fixed small but crippling bug in mahalanobis distance computation

--- measureOutliers.py
+++ measureOutliers.py
@@ -286,6 +286,6 @@ if(__name__=="__main__"):
     logMsg("Running normalized analysis")
     generateTimeSeriesLeave1("4year_features", use_link_db=True, normalize=True, make_zscore_vid=True)
     
-    #logMsg("Running weighted analysis")
-    #generateTimeSeriesLeave1("4year_features", use_link_db=True, use_feature_weights=True, normalize=True)
+    logMsg("Running weighted analysis")
+    generateTimeSeriesLeave1("4year_features", use_link_db=True, use_feature_weights=True, normalize=True)
     
